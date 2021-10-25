391b8f3c431d9d8a6f7f6739f7892712452f2608
Alin Manole <alin.manole@yahoo.com>
2021-10-25 15:45:51 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-
 import React from "react";
 import './App.css';
-import Steps from "./components/Steps";
+import Box from './components/Box'
+import './styles/global.css';
 
-const tempMin = -20 ;
-const temMax = 40 ;
-const heartMin = 80 ;
-const heartMax = 180 ;
-const stepsMin = 0 ;
-const stepsMax = 5000 ;
-
-
-
-
-
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="row">
-            <p>Heart : {heartMin}</p>
-            <p>Temperature : {tempMin}</p>
-            <p>Steps : {stepsMin}</p>
-            <Steps />
-        </div>    
+        <div className="row"> 
+          <p> Heart : {heartMin} </p>
+          <p> Temperatur : {tempMin} </p>
+          <p> Steps : {stepsMin} </p>
+          {/* Water  */}
+          <Box icon="local_drink" color="#3A85FF" value="1.5" unit="L" />
+
+          {/* Steps */} 
+          <Box icon="directions_walk" color="black" value="3000" unit="steps"/>
+
+          {/* Heart  */}
+          <Box icon="favorite" color="red" value="120" unit="bpm" />
+
+          {/* Temperature  */}
+          <Box icon="wb_sunny" color="yellow" value="-10" unit="°C" />
+        </div>
       </div>
     )
   }
 }
 
-export default App
+export default App;
